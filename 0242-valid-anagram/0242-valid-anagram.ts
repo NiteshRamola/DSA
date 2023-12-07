29fe37af53acab3317f69b4b6b9d9bb9d29c8ab6
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-07 22:51:35 +0530

    Time: 77 ms (51.87%), Space: 44.3 MB (89.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,20 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const charCount: Record<string, number> = {};
+
+    for (const char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (const char of t) {
+        if (!charCount[char]) {
+            return false;
+        }
+        charCount[char]--;
+    }
+
+    return Object.values(charCount).every(count => count === 0);
+}
