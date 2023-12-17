990ceff1aa3d2f001884d82f077fe150d8e465e1
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-17 19:44:11 +0530

    Time: 73 ms (69.25%), Space: 51.8 MB (59.58%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,16 @@
+function maxProfit(prices: number[]): number {
+    if (!prices || prices.length < 2) {
+        return 0;
+    }
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+}
