d298aacfb85b994fe36c398d5c4b0b6d1813d9dd
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-18 22:39:12 +0530

    Time: 53 ms (76.93%), Space: 42.3 MB (92.36%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,13 +1,15 @@
 function isSubsequence(s: string, t: string): boolean {
-    let i = 0;
-    let j = 0;
+    let index = 0;
 
-    while (i < s.length && j < t.length) {
-        if (s[i] === t[j]) {
-            i++;
+    for (const char of s) {
+        index = t.indexOf(char, index);
+
+        if (index === -1) {
+            return false;
         }
-        j++;
+
+        index++;
     }
 
-    return i === s.length;
+    return true;
 }
