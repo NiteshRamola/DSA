d985c1f4681c8785abc80b071a15074da3829519
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-20 20:49:19 +0530

    Time: 68 ms (25.93%), Space: 45 MB (62.00%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.ts
@@ -0,0 +1,18 @@
+function isHappy(n: number): boolean {
+    const seen: Set<number> = new Set();
+
+    while (n !== 1 && !seen.has(n)) {
+        seen.add(n);
+
+        let sum = 0;
+        while (n > 0) {
+            const digit = n % 10;
+            sum += digit * digit;
+            n = Math.floor(n / 10);
+        }
+
+        n = sum;
+    }
+
+    return n === 1;
+};
