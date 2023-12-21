1fdc41358c6efb3a0d4f2aeb9897d7ecc935ad76
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-21 20:01:01 +0530

    Time: 58 ms (90.55%), Space: 44.8 MB (57.32%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.ts
@@ -0,0 +1,27 @@
+function isIsomorphic(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sToTMapping = new Map<string, string>();
+    const tToSMapping = new Map<string, string>();
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        if (sToTMapping.has(charS)) {
+            if (sToTMapping.get(charS) !== charT) {
+                return false;
+            }
+        } else {
+            if (tToSMapping.has(charT) && tToSMapping.get(charT) !== charS) {
+                return false;
+            }
+            sToTMapping.set(charS, charT);
+            tToSMapping.set(charT, charS);
+        }
+    }
+
+    return true;
+}
