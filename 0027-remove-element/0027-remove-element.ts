b8c911a0bb4233fc3d93bebe77f056a2b2333df2
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-23 19:24:45 +0530

    Time: 58 ms (45.37%), Space: 44.3 MB (32.58%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,9 +1,8 @@
 function removeElement(nums: number[], val: number): number {
     let result = 0;
-    let length = nums.length
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < nums.length; i++){
         if(nums[i] !== val){
-            nums[result] = nums[i]
+            nums[result] = nums[i] 
             result++;
         }
     }
