66335fd6b13f6910eca3cb978f305f02c329534c
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-24 17:24:15 +0530

    Time: 56 ms (85.39%), Space: 45.7 MB (77.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,22 +1,18 @@
 function majorityElement(nums: number[]): number {
-    let candidate: number | null = null;
+    let candidate: number;
     let count = 0;
 
-    for (const num of nums) {
-        if (count === 0) {
-            candidate = num;
-        }
-
-        count += (num === candidate) ? 1 : -1;
-    }
-
-    count = 0;
-    for (const num of nums) {
-        if (num === candidate) {
+    for (let i = 0; i < nums.length; i++) {
+        if(count === 0){
+            candidate = nums[i];
+            count = 1;
+        }else if(candidate === nums[i]){
             count++;
+        }else{
+            count--;
         }
     }
-
-    return candidate!;
+    
+    return candidate
 
 };
