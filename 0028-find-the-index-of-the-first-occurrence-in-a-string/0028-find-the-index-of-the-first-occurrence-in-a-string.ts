ad0ac57c04cb71563358a43615e54486215631ef
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2023-12-25 11:00:37 +0530

    Time: 63 ms (17.44%), Space: 45.1 MB (5.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,3 +1,18 @@
 function strStr(haystack: string, needle: string): number {
-    return haystack.indexOf(needle)
+    const haystackLength = haystack.length;
+    const needleLength = needle.length;
+    
+    for(let i = 0; i <= haystackLength - needleLength; i++){
+        let str = "";
+        
+        for(let j=i; j < i + needleLength; j++){
+            str += haystack[j]
+        }
+        
+        if(str === needle){
+            return i
+        }
+    }
+    
+    return -1
 };
