bdb712e976ac9475397168b00778e5e927d3cf4a
Nitesh Ramola <niteshramola90@gmail.com>
2024-02-07 21:39:00 +0530

    insertionSort

--- src/practiceQuestions/array/sortArray.ts
+++ src/practiceQuestions/array/sortArray.ts
@@ -41,3 +41,47 @@ function bubbleSort(arr: number[]): number[] {
 }
 
 console.log("Bubble sort", bubbleSort([10, 20, 15, 2, 23, 90, 67]));
+
+// Insertion Sort
+function insertionSort(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    const temp = arr[i];
+
+    for (var j = i - 1; j >= 0 && temp < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
+
+console.log("Insertion sort", insertionSort([10, 20, 15, 2, 23, 90, 67]));
+
+// Merge Sort
+function mergeSort(arr: number[]): number[] {
+  return arr;
+}
+
+console.log("Merge sort", mergeSort([10, 20, 15, 2, 23, 90, 67]));
+
+// Quick Sort
+function quickSort(arr: number[]): number[] {
+  return arr;
+}
+
+console.log("Quick sort", quickSort([10, 20, 15, 2, 23, 90, 67]));
+
+// Heap Sort
+function heapSort(arr: number[]): number[] {
+  return arr;
+}
+
+console.log("Heap sort", heapSort([10, 20, 15, 2, 23, 90, 67]));
+
+// Radix Sort
+function radixSort(arr: number[]): number[] {
+  return arr;
+}
+
+console.log("Radix sort", radixSort([10, 20, 15, 2, 23, 90, 67]));
