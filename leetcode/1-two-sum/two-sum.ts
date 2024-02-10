4e3a15aafff5621917b7b2c6278f3f327170a1e9
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-02-10 19:42:19 +0530

    Time: 64 ms (68.53%) | Memory: 52.3 MB (23.42%) - LeetSync

---
+++ leetcode/1-two-sum/two-sum.ts
@@ -0,0 +1,15 @@
+function twoSum(nums: number[], target: number): number[] {
+        const numIndicesMap: { [key: number]: number } = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (numIndicesMap.hasOwnProperty(complement)) {
+            return [numIndicesMap[complement], i];
+        }
+
+        numIndicesMap[nums[i]] = i;
+    }
+
+    return null;
+};
