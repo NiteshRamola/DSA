cd0be144e1767ad338c850a2882c0ffcd04e1bb4
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-02-10 19:12:27 +0530

    Time: 333 ms (73.17%), Space: 103.2 MB (82.38%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.ts
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.ts
@@ -19,10 +19,10 @@ class RandomizedSet {
 
     remove(val: number): boolean {
         if (!this.valueToIndex.has(val)) {
-            return false; // Value does not exist
+            return false;
         }
 
-        const indexToRemove = this.valueToIndex.get(val);
+        const indexToRemove = this.valueToIndex.get(val)!;
         const lastValue = this.values[this.values.length - 1];
 
         this.values[indexToRemove] = lastValue;
