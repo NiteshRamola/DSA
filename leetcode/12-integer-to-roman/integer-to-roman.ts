cd9cbdeed10da7bed8e4f4358e5f6099e16799d3
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-02-13 21:01:37 +0530

    Time: 146 ms (20.20%) | Memory: 55.4 MB (37.23%) - LeetSync

--- leetcode/12-integer-to-roman/integer-to-roman.ts
+++ leetcode/12-integer-to-roman/integer-to-roman.ts
@@ -1,28 +1,50 @@
-function intToRoman(num: number): string {
-    const romanSymbols: [number, string][] = [
-        [1000, 'M'],
-        [900, 'CM'],
-        [500, 'D'],
-        [400, 'CD'],
-        [100, 'C'],
-        [90, 'XC'],
-        [50, 'L'],
-        [40, 'XL'],
-        [10, 'X'],
-        [9, 'IX'],
-        [5, 'V'],
-        [4, 'IV'],
-        [1, 'I'],
-    ];
+function intToRoman(n: number): string {
+    let r = '';
 
-    let result = '';
-
-    for (const [value, symbol] of romanSymbols) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+    while (true)
+    {
+        if (n - 1000 > -1) {
+            r += 'M';
+            n -= 1000;
+        } else if (n - 900 > -1) {
+            r += "CM";
+            n -= 900;
+        } else if (n - 500 > -1) {
+            r += 'D';
+            n -= 500;
+        } else if (n - 400 > -1) {
+            r += "CD";
+            n -= 400;
+        } else if (n - 100 > -1) {
+            r += 'C';
+            n -= 100;
+        } else if (n - 90 > -1) {
+            r += "XC";
+            n -= 90;
+        } else if (n - 50 > -1) {
+            r += 'L';
+            n -= 50;
+        } else if (n - 40 > -1) {
+            r += "XL";
+            n -= 40;
+        } else if (n - 10 > -1) {
+            r += 'X';
+            n -= 10;
+        } else if (n - 9 > -1) {
+            r += "IX";
+            n -= 9;
+        } else if (n - 5 > -1) {
+            r += 'V';
+            n -= 5;
+        } else if (n - 4 > -1) {
+            r += "IV";
+            n -= 4;
+        } else if (n - 1 > -1) {
+            r += 'I';
+            n -= 1;
+        } else {
+            break
         }
     }
-
-    return result;
+    return r
 };
