822785c3f9a79c23e8e93f67d409432af3a163b5
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-11 13:07:37 +0530

    Time: 53 ms (78.67%) | Memory: 51.4 MB (60.29%) - LeetSync

--- leetcode/2746-filter-elements-from-array/filter-elements-from-array.ts
+++ leetcode/2746-filter-elements-from-array/filter-elements-from-array.ts
@@ -1,13 +1,14 @@
 type Fn = (n: number, i: number) => any
 
 function filter(arr: number[], fn: Fn): number[] {
-    const result: number[] = [];
-
+    let currentIndex = 0;
     for(let i = 0; i < arr.length; i++){
         if(fn(arr[i], i)){
-            result.push(arr[i]);
+            arr[currentIndex] = arr[i];
+            currentIndex++;
         }
     }
-
-    return result;
+    
+    arr.length = currentIndex;
+    return arr;
 };
