8f61abd059a388d2e1b205b97aef96fe3308c33a
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-11 12:58:26 +0530

    Time: 62 ms (24.83%) | Memory: 51.3 MB (51.71%) - LeetSync

--- leetcode/2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
+++ leetcode/2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
@@ -1,9 +1,8 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const result: number[] = [];
 
     for(let i = 0; i < arr.length; i++){
-        result.push(fn(arr[i], i))
+       arr[i] = (fn(arr[i], i))
     }
 
-    return result;
+    return arr;
 };
