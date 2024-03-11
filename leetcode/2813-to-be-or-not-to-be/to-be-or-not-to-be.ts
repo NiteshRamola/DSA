c04f84a3283195198902491038bd9d10222b0d52
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-11 12:16:20 +0530

    Time: 54 ms (67.92%) | Memory: 51.1 MB (9.50%) - LeetSync

---
+++ leetcode/2813-to-be-or-not-to-be/to-be-or-not-to-be.ts
@@ -0,0 +1,26 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (value) => {
+            if(val !== value){
+                throw new Error("Not Equal")
+            }
+            return true;
+        },
+        notToBe: (value) => {
+            if(val === value){
+                throw new Error("Equal")
+            }
+            return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
