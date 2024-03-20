16402f3afa1246f93ffa002d234b409b0c546633
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-20 16:30:48 +0530

    Time: 76 ms (14.71%) | Memory: 55.9 MB (7.84%) - LeetSync

--- leetcode/229-majority-element-ii/majority-element-ii.ts
+++ leetcode/229-majority-element-ii/majority-element-ii.ts
@@ -1,22 +1,43 @@
 function majorityElement(nums: number[]): number[] {
-    let obj = {};
-    const arr = [];
-    for(let i = 0; i < nums.length; i++){
-        if(obj[nums[i]]){
-            ++obj[nums[i]].count;
-        }else {
-            obj[nums[i]] = {count: 1, pushed: false};
-        }
+    let candidate1: number | null = null;
+    let candidate2: number | null = null;
+    let count1: number = 0;
+    let count2: number = 0;
 
-        if(!obj[nums[i]].pushed && obj[nums[i]].count > (nums.length/3)){
-            arr.push(nums[i]);
-            obj[nums[i]].pushed = true;
-
-            if(arr.length === 2){
-                break;
-            }
+    for (const num of nums) {
+        if (candidate1 === num) {
+            count1++;
+        } else if (candidate2 === num) {
+            count2++;
+        } else if (count1 === 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 === 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
         }
     }
-    
-    return arr;
+
+    count1 = 0;
+    count2 = 0;
+    for (const num of nums) {
+        if (num === candidate1) {
+            count1++;
+        } else if (num === candidate2) {
+            count2++;
+        }
+    }
+
+    const result: number[] = [];
+    if (count1 > nums.length / 3) {
+        result.push(candidate1);
+    }
+    if (count2 > nums.length / 3) {
+        result.push(candidate2);
+    }
+
+    return result;
 };
