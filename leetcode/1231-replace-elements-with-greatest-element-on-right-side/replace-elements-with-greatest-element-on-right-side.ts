a73f7c1c1a4ec2bc10a0abb02b9dc5684f90eab3
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-27 12:22:03 +0530

    Time: 143 ms (53.73%) | Memory: 59.4 MB (57.65%) - LeetSync

---
+++ leetcode/1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
@@ -0,0 +1,12 @@
+function replaceElements(arr: number[]): number[] {
+    let max = arr[arr.length - 1];
+    arr[arr.length - 1] = -1;
+    
+    for(let i = arr.length - 2; i >= 0; i--) {
+        let curr = arr[i];
+        arr[i] = max;
+        if(curr > max) max = curr;
+    }
+    
+    return arr;
+};
