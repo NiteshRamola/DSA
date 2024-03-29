e6067edd8a7a4fff7e3ea742029a981043ab2b32
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-03-29 11:45:03 +0530

    Time: 48 ms (93.42%) | Memory: 51.9 MB (8.32%) - LeetSync

--- leetcode/27-remove-element/remove-element.ts
+++ leetcode/27-remove-element/remove-element.ts
@@ -1,11 +1,11 @@
 function removeElement(nums: number[], val: number): number {
-    let result = 0;
-    for (let i = 0; i < nums.length; i++){
-        if(nums[i] !== val){
-            nums[result] = nums[i] 
-            result++;
-        }
+    let length = 0;
+    for(let i = 0; i < nums.length; i++){
+      if(nums[i] !== val){
+        nums[length] = nums[i];
+        length++;
+      }
     }
-    
-    return result;
+
+    return length;
 };
