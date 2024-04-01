366a78c59ca3e93007998aa236fae1b3fc7e67e9
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-04-01 11:58:54 +0530

    Time: 70 ms (32.00%) | Memory: 55.1 MB (12.27%) - LeetSync

--- leetcode/169-majority-element/majority-element.ts
+++ leetcode/169-majority-element/majority-element.ts
@@ -1,11 +1,11 @@
 function majorityElement(nums: number[]): number {
-    let obj = {}
-    for (const num of nums) {
-        const count = (obj[num] || 0) + 1;
-        obj[num] = count;
+  const obj: { [key: number]: number } = {};
 
-        if (count > nums.length / 2) {
-            return num;
-        }
+  for(let num of nums){
+    obj[num] = (obj[num] || 0) + 1;
+
+    if (obj[num] > nums.length / 2) {
+      return num;
     }
+  }
 };
