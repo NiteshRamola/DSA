f48d63f567e3900d74332963c7dd0e50913bf305
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-04-02 11:57:06 +0530

    Time: 96 ms (76.81%) | Memory: 59.4 MB (27.71%) - LeetSync

--- leetcode/303-range-sum-query-immutable/range-sum-query-immutable.ts
+++ leetcode/303-range-sum-query-immutable/range-sum-query-immutable.ts
@@ -1,17 +1,14 @@
 class NumArray {
-  private nums: number[];
+  private prefixSum: number[];
   constructor(nums: number[]) {
-    this.nums = nums;
+    this.prefixSum = new Array(nums.length + 1).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
+    }
   }
 
   sumRange(left: number, right: number): number {
-    let sum: number = 0;
-
-    for(let i = left; i <= right; i++){
-      sum += this.nums[i];
-    }
-
-    return sum;
+    return this.prefixSum[right+1] - this.prefixSum[left];
   }
 }
 
