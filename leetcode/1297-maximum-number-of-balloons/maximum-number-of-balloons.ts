7998f61bf1ad5d299cc3f7f83b798f5829b7fa8f
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-04-04 14:53:51 +0530

    Time: 76 ms (11.69%) | Memory: 53.1 MB (38.31%) - LeetSync

--- leetcode/1297-maximum-number-of-balloons/maximum-number-of-balloons.ts
+++ leetcode/1297-maximum-number-of-balloons/maximum-number-of-balloons.ts
@@ -1,19 +1,18 @@
 function maxNumberOfBalloons(text: string): number {
-  let countText = {};
-  let balloon = {};
-  let result = text.length;
-
-  for (let ch of text) {
-      countText[ch] = (countText[ch] || 0) + 1;
+  const counts = new Map()
+  const characters = ["b", "a", "l", "o", "n"]
+  
+  for (const c of text) {
+    if (characters.includes(c)) {
+      counts.set(c, (counts.get(c) || 0) + 1)
+    }
   }
-
-  for (let ch of 'balloon') {
-      balloon[ch] = (balloon[ch] || 0) + 1;
-  }
-
-  for (let c in balloon) {
-      result = Math.min(result, Math.floor((countText[c] || 0) / balloon[c]));
-  }
-
-  return result;
+  
+  const b = counts.get("b") || 0
+  const a = counts.get("a") || 0
+  const l = Math.floor((counts.get("l") || 0) / 2)
+  const o = Math.floor((counts.get("o") || 0) / 2)
+  const n = counts.get("n") || 0
+  
+  return Math.min(b, a, l, o, n)
 }
