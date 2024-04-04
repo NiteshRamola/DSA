8974b229df1f16af31fa0b61fb791d64bc16c819
Nitesh Ramola <56308165+NiteshRamola@users.noreply.github.com>
2024-04-04 12:01:14 +0530

    Time: 64 ms (22.68%) | Memory: 50.8 MB (56.22%) - LeetSync

--- leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
+++ leetcode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,3 +1,20 @@
 function strStr(haystack: string, needle: string): number {
-    return haystack.indexOf(needle);
+  for(let i = 0; i < haystack.length; i++){
+    if(haystack[i] === needle[0]){
+      let isSame = true;
+      for(let j = 0; j < needle.length; j++){
+        if(needle[j] !== haystack[i + j]) {
+          isSame = false;
+          break;
+        };
+      }
+
+      if(isSame){
+        return i;
+      }
+
+    }
+  }
+
+  return -1;
 };
